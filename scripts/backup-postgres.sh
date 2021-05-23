3741edf3d8419c97ba642c0aaca64efd5b6dac31
Tom Beynon <tom@tombeynon.co.uk>
2021-05-23 12:36:44 +0100

    Remove old backup files

--- scripts/backup-postgres.sh
+++ scripts/backup-postgres.sh
@@ -20,21 +20,41 @@ pg_dump --format=custom \
         > db.dump
 
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${FILEBASE_BUCKET}/backups/${POSTGRES_DATABASE}_${timestamp}.dump"
+s3_uri_base="s3://${FILEBASE_BUCKET}/backups"
+aws_args="--endpoint-url https://s3.filebase.com"
+
+s3_uri="${s3_uri_base}/${POSTGRES_DATABASE}_${timestamp}.dump"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
   gpg --symmetric --batch --passphrase "$PASSPHRASE" db.dump
   rm db.dump
   local_file="db.dump.gpg"
-  s3_uri="${s3_uri_base}.gpg"
+  s3_uri="${s3_uri}.gpg"
 else
   local_file="db.dump"
-  s3_uri="$s3_uri_base"
+  s3_uri="$s3_uri"
 fi
 
 echo "Uploading backup to $FILEBASE_BUCKET..."
-aws --endpoint-url https://s3.filebase.com s3 cp "$local_file" "$s3_uri"
+aws $aws_args s3 cp "$local_file" "$s3_uri"
 rm "$local_file"
+
+deleteAfter="${KEEP_BACKUPS:-"10 days"}"
+echo "Deleting backups older than $deleteAfter"
+
+aws $aws_args s3 ls "${s3_uri_base}/${POSTGRES_DATABASE}" | while read -r line;  do
+createDate=`echo $line|awk {'print $1" "$2'}`
+createDate=`date -d"$createDate" +%s`
+olderThan=`date -d"-$deleteAfter" +%s`
+if [[ $createDate -lt $olderThan ]]
+then
+  fileName=`echo $line|awk '{$1=$2=$3=""; print $0}' | sed 's/^[ \t]*//'`
+  if [[ $fileName != "" ]]
+  then
+    aws $aws_args s3 rm "${s3_uri_base}/$fileName"
+  fi
+fi
+done;
 
 echo "Backup complete."
